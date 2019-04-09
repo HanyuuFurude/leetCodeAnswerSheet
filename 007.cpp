ad44adde47ad929fbc9da9eafc00599a9d9a4be7
Hanyuu Furude <HanyuuFurude@outlook.com>
2019-04-09 16:34:16 +0800

    20190409Add007

---
+++ 007.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+  public:
+	int reverse(int x)
+	{
+		//cout<<INT_MAX<<endl;
+		//cout<<INT_MIN<<endl;
+		//cout<<x<<endl;
+		long org = x;
+		long tmp = 0;
+		while (org != 0)
+		{
+			tmp *= 10;
+			tmp += org % 10;
+			org /= 10;
+			//cout<<tmp<<endl;
+		}
+		if (tmp > INT_MAX || tmp < INT_MIN)
+			return 0;
+		return tmp;
+	}
+};
