0ec84018782ff91261ebef0583ed36fc285bcfec
HanyuuuLu <HanyuuFurude@outlook.com>
2020-01-24 13:43:18 +0800

    202001241343

---
+++ src/024.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+using Number024;
+public partial class Solution
+{
+    public ListNode SwapPairs(ListNode head)
+    {
+        ListNode res, ptr, a, b;
+        res = new ListNode(0);
+        ptr = res;
+        ptr.next = head;
+        while(ptr!=null && ptr.next!=null)
+        {
+            a = ptr.next;b = a.next;
+            ptr.next = b;a.next = b.next;b.next = a;
+            ptr = a;
+        }
+        return res.next;
+    }
+}
+namespace Number024
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        { val = x; }
+    }
+}
