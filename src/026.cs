b0db68a4e3b60d05d77184f3722b5e782788c6d5
HanyuuuLu <HanyuuFurude@outlook.com>
2020-01-26 12:48:26 +0800

    202001261248

---
+++ src/026.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public partial class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int count = nums.Length;
+        if(count<1)
+        { return count; }
+        int i = 1;
+        int k = 1;
+        while(i<count)
+        {
+            if(nums[i]==nums[i-1])
+            {++i;}
+            else
+            { nums[k] = nums[i];++i;++k; }
+        }
+        return k;
+    }
+}
