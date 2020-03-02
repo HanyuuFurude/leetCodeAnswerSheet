9d8616e9280421eb037ba80476f4ff8b9258b33c
HanyuuuLu <HanyuuFurude@outlook.com>
2020-03-03 00:17:25 +0800

    202003030017

---
+++ src/115.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public partial class Solution
+{
+    public int NumDistinct(string s, string t)
+    {
+        int[][] dp = new int[t.Length + 1][];
+        for (var i = 0; i <= t.Length; ++i)
+        { dp[i] = new int[s.Length + 1]; }
+        for (var j = 0; j < s.Length + 1; ++j)
+        { dp[0][j] = 1; }
+        for (var i = 1; i < t.Length + 1; ++i)
+        {
+            for (var j = 1; j < s.Length + 1; ++j)
+            {
+                if (t[i - 1] == s[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[t.Length][s.Length];
+    }
+}
---
+++ src/117.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PublicClass;
+
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+*/
+public partial class Solution
+{
+    public Node Connect(Node root)
+    {
+        if (root == null)
+        { return root; }
+        Queue<Node> Q = new Queue<Node>();
+        Q.Enqueue(root);
+        while (Q.Count > 0)
+        {
+            int size = Q.Count;
+            for (var i = 0; i < size; ++i)
+            {
+                Node node = Q.Dequeue();
+                if (i < size - 1)
+                {
+                    node.next = Q.Peek();
+                }
+                if (node.left != null)
+                {
+                    Q.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    Q.Enqueue(node.right);
+                }
+            }
+        }
+        return root;
+    }
+}
