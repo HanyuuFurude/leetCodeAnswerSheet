0c7a7f9664895001a4e18ed9a36ac8ef1a35836d
Hanyuu Lu <HanyuuFurude@outlook.com>
2020-03-18 23:59:34 +0800

    202003182359

---
+++ src/144.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        node, output = root, []
+        while node:  
+            if not node.left: 
+                output.append(node.val)
+                node = node.right 
+            else: 
+                predecessor = node.left 
+
+                while predecessor.right and predecessor.right is not node: 
+                    predecessor = predecessor.right 
+
+                if not predecessor.right:
+                    output.append(node.val)
+                    predecessor.right = node  
+                    node = node.left  
+                else:
+                    predecessor.right = None
+                    node = node.right         
+
+        return output
