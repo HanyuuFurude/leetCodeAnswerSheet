0ad9b8cd74037cbe8bb8d004d4106c9718c7d4f3
HanyuuLu <HanyuuLu@outlook.com>
2020-11-25 01:33:14 +0800

    S222

--- java/src/main/java/hanyuu/solutions/S222.java
+++ java/src/main/java/hanyuu/solutions/S222.java
@@ -17,7 +17,35 @@ public interface S222 {
 
         @Override
         public int countNodes(TreeNode root) {
-            return 0;
+            if (root==null){
+                return 0;
+            }
+            // 获取树高度
+            int height = 0;
+            TreeNode node = root;
+            while (node.left!=null){
+                ++height;
+                node = node.left;
+            }
+            int l = 1<<height,r = (1<<(height+1))-1;
+            while (l<r){
+                int m = (r-l+1)/2+l;
+
+            }
+            return l;
+        }
+        public boolean exists(TreeNode root,int height,int src){
+            int bits = 1<<(height-1);
+            TreeNode node = root;
+            while (node!=null && bits>0){
+                if((bits & src) == 0){
+                    node = node.left;
+                }else {
+                    node = node.right;
+                }
+                bits>>=1;
+            }
+            return node!=null;
         }
     }
     
